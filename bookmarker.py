d106aecf04052987139f9547a74e3b51b92cb240
Andrea Bergia <andreabergia@gmail.com>
2012-01-24 18:44:12 +0100

    Refactored the model out of the main .py file

--- bookmarker.py
+++ bookmarker.py
@@ -1,62 +1,29 @@
+import model
+
 import webapp2
 import json
-import logging
 import datetime
 import os
 from common.templatefilters import formatDateDistance
 
-from google.appengine.ext import db
 from google.appengine.api import users
-from google.appengine.api import memcache
 from google.appengine.ext.webapp import template
 
 
-class Link(db.Model):
-    url = db.LinkProperty()
-    user = db.UserProperty()
-    dateInsert = db.DateTimeProperty(auto_now_add=True)
-
-
-def dbLinkToStoredObject(link):
-    return {"url" : link.url, "id": str(link.key()), "dateInsert" : link.dateInsert}
-
-
-def get_memcache_key(user):
-    return 'user_list_' + user.user_id()
-
-
-def toJson(object):
-    class JsonEncoder(json.JSONEncoder):
-        def default(self, obj):
-            if isinstance(obj, datetime.datetime):
-                return formatDateDistance(obj)
-            return json.JSONEncoder.default(self, obj)
-    return json.dumps(object, cls=JsonEncoder)
-
-
 class List(webapp2.RequestHandler):
-    def getLinks(self):
+    def get(self):
+        def toJson(object):
+            class JsonEncoder(json.JSONEncoder):
+                def default(self, obj):
+                    if isinstance(obj, datetime.datetime):
+                        return formatDateDistance(obj)
+                    return json.JSONEncoder.default(self, obj)
+            return json.dumps(object, cls=JsonEncoder)
+
         user = users.get_current_user()
         assert user is not None, 'no current user!'
 
-        # Try to hit the memcache
-        urls = memcache.get(get_memcache_key(user))
-        if urls is None:
-            # No such luck... We'll need to go to the data store
-            urls = []
-            links = db.GqlQuery('SELECT * '
-                                'FROM Link '
-                                'WHERE user = :1',
-                                user)
-            for link in links:
-                urls.append(dbLinkToStoredObject(link))
-            if not memcache.add(get_memcache_key(user), urls, 360):
-                logging.error('Memcache set failed!')
-
-        return urls
-
-    def get(self):
-        urls = self.getLinks()
+        urls = model.getLinks(user)
 
         self.response.headers['Content-Type'] = 'application/json'
         respJSON = toJson(urls)
@@ -68,21 +35,7 @@ class Add(webapp2.RequestHandler):
         user = users.get_current_user()
         assert user is not None, 'no current user!'
 
-        url = self.request.get('url')
-
-        link = Link()
-        link.url = db.Link(url)
-        link.user = user
-        link.put()
-
-        # Try to update the memcache
-        memcache_key = get_memcache_key(user)
-        urls = memcache.get(memcache_key)
-        if urls is not None:
-            urls.append(dbLinkToStoredObject(link))
-            if not memcache.replace(memcache_key, urls, 360):
-                logging.error('Memcache set failed!')
-                memcache.delete(memcache_key)
+        link = model.addLinkForUser(user, request.get('url'))
 
         respJSON = toJson( {'ok': True, 'id': str(link.key())} )
         self.response.headers['Content-Type'] = 'application/json'
@@ -95,20 +48,7 @@ class Remove(webapp2.RequestHandler):
         assert user is not None, 'no current user!'
 
         linkKey = self.request.get('id')
-
-        link = Link.get(linkKey)
-        if link:
-            link.delete()
-
-            # Try to update the memcache
-            memcache_key = get_memcache_key(user)
-            urls = memcache.get(memcache_key)
-            if urls is not None:
-                urls = [link for link in urls if link['id'] != linkKey]
-                if not memcache.replace(memcache_key, urls, 360):
-                    logging.error('Memcache set failed!')
-                    memcache.delete(memcache_key)
-
+        if model.delLinkForUser(user, linkKey):
             self.response.headers['Content-Type'] = 'application/json'
             self.response.out.write('{"ok" : true}')
         else:
@@ -117,9 +57,10 @@ class Remove(webapp2.RequestHandler):
 
 class Links(webapp2.RequestHandler):
     def get(self):
-        l = List()
-        urls = l.getLinks()
+        user = users.get_current_user()
+        assert user is not None, 'no current user!'
 
+        urls = model.getLinks(user)
         template_values = {'urls' : urls}
 
         path = os.path.join(os.path.join(os.path.dirname(__file__), 'static'), 'list.html')
---
+++ model.py
@@ -0,0 +1,80 @@
+import logging
+
+from google.appengine.ext import db
+from google.appengine.api import memcache
+
+
+class Link(db.Model):
+    url = db.LinkProperty()
+    user = db.UserProperty()
+    dateInsert = db.DateTimeProperty(auto_now_add=True)
+
+
+def dbLinkToStoredObject(link):
+    return {'url' : link.url, 'id': str(link.key()), 'dateInsert' : link.dateInsert}
+
+
+def get_memcache_key(user):
+    return 'user_list_' + user.user_id()
+
+
+def getLinks(user):
+    assert user is not None, 'User is none!'
+
+    # Try to hit the memcache
+    urls = memcache.get(get_memcache_key(user))
+    if urls is None:
+        # No such luck... We'll need to go to the data store
+        urls = []
+        links = db.GqlQuery('SELECT * '
+                            'FROM Link '
+                            'WHERE user = :1',
+                            user)
+        for link in links:
+            urls.append(dbLinkToStoredObject(link))
+        if not memcache.add(get_memcache_key(user), urls, 360):
+            logging.error('Memcache set failed!')
+
+    return urls
+
+
+def addLinkForUser(user, url):
+    assert user is not None, 'User is none!'
+    assert url is not None, 'url is none!'
+
+    link = Link()
+    link.url = db.Link(url)
+    link.user = user
+    link.put()
+
+    # Try to update the memcache
+    memcache_key = get_memcache_key(user)
+    urls = memcache.get(memcache_key)
+    if urls is not None:
+        urls.append(dbLinkToStoredObject(link))
+        if not memcache.replace(memcache_key, urls, 360):
+            logging.error('Memcache set failed!')
+            memcache.delete(memcache_key)
+
+    return link
+
+def delLinkForUser(user, linkId):
+    assert user is not None, 'User is none!'
+    assert linkId is not None, 'linkId is none!'
+
+    link = Link.get(linkKey)
+    if link:
+        link.delete()
+
+        # Try to update the memcache
+        memcache_key = get_memcache_key(user)
+        urls = memcache.get(memcache_key)
+        if urls is not None:
+            urls = [link for link in urls if link['id'] != linkKey] # Remove the link from the array
+            if not memcache.replace(memcache_key, urls, 360):
+                logging.error('Memcache set failed!')
+                memcache.delete(memcache_key)
+
+        return True
+    else:
+        return False
